321967fcad8f0071edd1a59ba6f4eadcbb6a06fc
daniel-z-johnson <danzaljohnson@gmail.com>
2020-04-16 11:04:44 -0600

    added method , ran go init mod for dependencies

---
+++ go.mod
@@ -0,0 +1,8 @@
+module daniel-z-johnson/journal-of-self
+
+go 1.14
+
+require (
+	github.com/google/uuid v1.1.1
+	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904
+)
--- models/user.go
+++ models/user.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User represents, well, a user
@@ -17,4 +19,20 @@ type User struct {
 	UpdatedAt time.Time
 	// will be a generated icon, the user will be able to re-generated several times a day, value will be location
 	Icon string
+}
+
+// CreateUser - from the required fields create a User
+func CreateUser(username string, email string, password string, icon string) *User {
+	return &User{
+		Username:  username,
+		Email:     email,
+		Password:  hashPassword(password),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Icon:      icon,
+	}
+}
+
+func hashPassword(password string) string {
+	return bcrypt.GenerateFromPassword([]byte(password))
 }

04b13dfbbd561638dd89b08983e0ef5965172d7e
daniel-z-johnson <danzaljohnson@gmail.com>
2022-06-09 18:34:50 -0600

    stating user struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/daniel-z-johnson/journal-of-self
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.7
--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
 )
 
 func main() {
-    fmt.Println("Application Start")
+	fmt.Println("Application Start")
+	application()
 }
 
+func application() error {
+	r := chi.NewMux()
+	r.HandleFunc("/test", test)
+	return http.ListenAndServe(":1117", r)
+}
+
+func test(resp http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(resp, "Success")
+}

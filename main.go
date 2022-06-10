7a187a4111db125bf8bdd65d8f1ee9ae7d0dd4f0
daniel-z-johnson <danzaljohnson@gmail.com>
2022-06-09 18:20:06 -0600

    Starting over

--- main.go
+++ main.go
@@ -1,43 +1,10 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-
-	"github.com/daniel-z-johnson/journal-of-self/config"
-	"github.com/daniel-z-johnson/journal-of-self/controllers"
-	"github.com/daniel-z-johnson/journal-of-self/models"
-	"github.com/daniel-z-johnson/journal-of-self/views"
 )
 
 func main() {
-	migrate := flag.Bool("migrate", false, "If set, will do migration")
-	flag.Parse()
-
-	fmt.Println("Application 'Journal-of-self' start")
-	conConfig, err := config.LoadConfig("config.json")
-	if err != nil {
-		panic(err)
-	}
-	services, err := models.NewServices(conConfig.Database, *migrate)
-	defer services.Close()
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	u, err := models.CreateUser("a", "a", "a", "a")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(services.UserService.Insert(*u))
-	fmt.Println(services)
-
-	uc := controllers.NewUserController(services.UserService)
-	r := chi.NewRouter()
-	r.Get("/users", uc.Signup)
-	r.Handle("/static/*", views.StaticFilesServer())
-	http.ListenAndServe(":1117", r)
+    fmt.Println("Application Start")
 }
+
